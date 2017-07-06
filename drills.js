1321ce094e72db80d61465ebb6d00ae5e2595fc3
zwillerzoog <marsh.heidi.c@gmail.com>
2017-07-06 14:10:07 -0400

    second push

--- drills.js
+++ drills.js
@@ -2,11 +2,13 @@ const knex = require('knex')({
   client: 'pg',
   connection: {
     //database: 'dev-restaurants-app'
-    database: 'dev-restaurants-app'
+    database: 'dev-restaurants-app',
+    user: 'dev',
+    password: 'anschel1'
   },
 });
 
-const { DATABASE, PORT } = require('./config');
+//const { DATABASE, PORT } = require('./config');
 // const knex = require('knex')({
 //   client: 'pg',
 //   connection: '	postgres://tdqonxzs:cAAs-TOvQBFiExdgFaZRKQJiCXF1o2QC@stampy.db.elephantsql.com:5432/tdqonxzs'
@@ -15,40 +17,110 @@ const { DATABASE, PORT } = require('./config');
 // clear the console before each run
 process.stdout.write('\033c');
 
-//1. Get All Restaurants
-knex.select()
-  .table('restaurants')
-  .limit(4)
-  .then(results => console.log('Get All Restaurants w/ limit 4: ', results));
+// //1. Get All Restaurants
+// knex.select()
+//   .table('restaurants')
+//   .limit(4)
+//   .then(results => console.log('Get All Restaurants w/ limit 4: ', results));
 
-// 2. Get Italian restaurants
-knex.select()
-  .from('restaurants')
-  .where('cuisine', 'Italian')
-  .limit(4)
-  .then(results => console.log('Get Italian restaurants /w limit 4: ', results));
+// // 2. Get Italian restaurants
+// knex.select()
+//   .from('restaurants')
+//   .where('cuisine', 'Italian')
+//   .limit(4)
+//   .then(results => console.log('Get Italian restaurants /w limit 4: ', results));
 
-// 3. Get 10 Italian restaurants, subset of fields
+// // 3. Get 10 Italian restaurants, subset of fields
+// knex.select('id', 'name')
+//   .from('restaurants')
+//   .where('cuisine', 'Italian')
+//   .limit(10)
+//   .then(results => console.log('Get 10 Italian restaurants, subset of fields: ', results));
+
+// // 4. Count of Thai restaurants
+// knex.count('id')
+//   .from('restaurants')
+//   .where('cuisine', 'Thai')
+//   .then(results => console.log('Count of Thai restaurants: ', results));
+
+// // 5. Count of restaurants
+
+// knex.count('id')
+//   .from('restaurants')
+//   .then(results => console.log('Count of restaurants: ', results));
+
+// // 6. Count of Thai restaurants in zip code
+
+// knex.count()
+//   .from('restaurants')
+//   .where({'cuisine': 'Thai', 'address_zipcode': '11372'})
+//   .then(results => console.log('Count of Thai restaurants in zip code 11372: ', results));
+
+// 7. Italian restaurants in one of several zip codes
+
 knex.select('id', 'name')
   .from('restaurants')
-  .where('cuisine', 'Italian')
-  .limit(10)
-  .then(results => console.log('Get 10 Italian restaurants, subset of fields: ', results));
+  .where({'cuisine': 'Italian', 'address_zipcode': ['10012', '10013', '10014']})
+  .orderBy('name')
+  .limit(5)
+  .then(results => console.log('Italian restaurants in one of several zip codes: ', results));
 
-// 4. Count of Thai restaurants
-knex.count('id')
-  .from('restaurants')
-  .where('cuisine', 'Thai')
-  .then(results => console.log('Count of Thai restaurants: ', results));
+// // 8. Create a restaurant
 
-// 5. Count of restaurants
+// knex
+//   .insert({
+//     name: 'Byte Cafe',
+//     borough: 'Brooklyn',
+//     cuisine: 'coffee',
+//     address_building_number: '123',
+//     address_street: 'Atlantic Avenue',
+//     address_zipcode: '11231'
+//   })
+//   .into('restaurants')
+//   .then(results => console.log('Create a restaurant: ', results));
 
+// // 9. Create a restaurant and return id and name
 
+// knex
+//   .insert({
+//     name: 'Bat Cafe',
+//     borough: 'Brooklyn',
+//     cuisine: 'coffee',
+//     address_building_number: '123',
+//     address_street: 'Atlantic Avenue',
+//     address_zipcode: '11231'
+//   })
+//   .into('restaurants')
+//   .returning(['id', 'name'])
+//   .then(results => console.log('Create a restaurant and return id and name: ', results));
 
+//   // 10. Create three restaurants and return id and name
 
-
-
-
+//   knex
+//     .insert({
+//     name: 'Donut Cafe',
+//     borough: 'Brooklyn',
+//     cuisine: 'coffee',
+//     address_building_number: '123',
+//     address_street: 'Atlantic Avenue',
+//     address_zipcode: '11231'
+//   },
+//   {
+//     name: 'Brooklyn Cafe',
+//     borough: 'Brooklyn',
+//     cuisine: 'coffee',
+//     address_building_number: '123',
+//     address_street: 'Atlantic Avenue',
+//     address_zipcode: '11231'
+//   },
+//   {
+//     name: 'Bagel Cafe',
+//     borough: 'Brooklyn',
+//     cuisine: 'coffee',
+//     address_building_number: '123',
+//     address_street: 'Atlantic Avenue',
+//     address_zipcode: '11231'
+//   })
 
 
 
